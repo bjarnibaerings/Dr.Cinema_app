7162ba523e3456aa015d1e11c55a1fa9b7d6bc47
Manwin <manwin8@gmail.com>
2024-12-09 13:39:17 +0000

    asjdhajowdn wo iowdn iow

--- Dr_Cinema/App.js
+++ Dr_Cinema/App.js
@@ -2,13 +2,11 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider as storeProvider } from 'react-redux';
 import store from "./src/Redux/store";
+import AppContainer from "./src/Routes"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AppContainer/>
   );
 }
 
