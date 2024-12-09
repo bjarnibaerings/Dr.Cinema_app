3d967533514948f985e86bfc6ad71cdd577fa37d
bjarnibaerings <bjarnibh@outlook.com>
2024-12-09 11:10:14 +0000

    dfgdsg

--- Dr_Cinema/App.js
+++ Dr_Cinema/App.js
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider as storeProvider } from 'react-redux';
+import store from "./src/Redux/store";
 
 export default function App() {
   return (
