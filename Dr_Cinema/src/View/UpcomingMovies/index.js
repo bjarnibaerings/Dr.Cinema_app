9dbb08791de870bdd50345aac8d82293f958cae9
telmathorhalls <telmat23@ru.is>
2024-12-10 10:15:46 +0000

    i just want to puuulllll

--- Dr_Cinema/src/View/UpcomingMovies/index.js
+++ Dr_Cinema/src/View/UpcomingMovies/index.js
@@ -1,19 +1,4 @@
-import React from "react"
-import {View, Text, TouchableOpacity} from "react-native"
-
-
-const UpcomingMovies = ({navigation}) => {
-    return(
-        <View>
-            <Text> this is UpcomingMovies </Text>
-            <TouchableOpacity onPress={() => navigation.navigate("{upcomingMovie.name}")}>
-                <Text>press here to go to chosen upcoming movive</Text>
-            </TouchableOpacity>
-        </View>
-    )
-};
-
-export default UpcomingMovies;import React, { useEffect } from "react";
+import React, { useEffect } from "react";
 import { View, Text, TouchableOpacity, Alert } from "react-native";
 import { useDispatch } from "react-redux";
 import { getUpcomingMovies } from "../../Actions/upcomingMoviesActions"
@@ -27,6 +12,10 @@ const UpcomingMoviesScreen = () => {
     return (
         <View>
             <Text>Here we will have all upcoming movies</Text>
+            <Text> this is UpcomingMovies </Text>
+            <TouchableOpacity onPress={() => navigation.navigate("{upcomingMovie.name}")}>
+                <Text>press here to go to chosen upcoming movive</Text>
+            </TouchableOpacity>
         </View>
     )
 };
