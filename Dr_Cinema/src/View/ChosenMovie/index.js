dc738de591491f910df89155d6fd597c0e49821a
Manwin <manwin8@gmail.com>
2024-12-12 14:15:19 +0000

    footer styling

--- Dr_Cinema/src/View/ChosenMovie/index.js
+++ Dr_Cinema/src/View/ChosenMovie/index.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { View } from "react-native";
 import DetailedMovie from "../../Components/Movies/detailedlMovie";
+import Footer from "../../Components/Footer";
 
 const MovieScreen = ({ navigation, route }) => {
     console.log(route)
     return (
-        <View>
+        <View style = {{flex:1}}>
+            <View style = {{flex:1}}>
             <DetailedMovie route = {route}/> {/* render compinent and passing right route params */}
+            </View>
+            <Footer/>
         </View>
     );
 };
