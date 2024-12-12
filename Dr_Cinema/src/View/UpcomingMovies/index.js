d43f69fb11a95b473f4748d20ce2af4c1aa127db
Manwin <manwin8@gmail.com>
2024-12-12 16:15:20 +0000

    fixed to watch youtube video on phone

--- Dr_Cinema/src/View/UpcomingMovies/index.js
+++ Dr_Cinema/src/View/UpcomingMovies/index.js
@@ -1,79 +1,68 @@
-import React, { useEffect, Linking, useState } from "react";
-import { View, Text, TouchableOpacity, Alert, FlatList, Image,  Button, Modal } from "react-native";
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigation } from '@react-navigation/native';
-import { getUpcomingMovies } from "../../Actions/upcomingMoviesActions"
-import styles from "./styles"
-import Video from "react-native-video"
+import React, { useState, useEffect } from 'react';
+import { View, Text, TouchableOpacity, Alert, Modal, Button, Image } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUpcomingMovies } from "../../Actions/upcomingMoviesActions";
+import styles from './styles';
+import YouTubeVideoPlayer from "../../Components/YoutubePlayer";
 
 const UpcomingMoviesScreen = () => {
-    const dispatch = useDispatch();
-    const navigation = useNavigation();
-    const { data: upcomingMovies } = useSelector(state => state.upcomingMovies);
+  const dispatch = useDispatch();
+  const { data: upcomingMovies = [], loading, error } = useSelector(
+    state => state.upcomingMovies || {}
+  );
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
-    const [isModalVisible, setModalVisible] = useState(false);
-    const [trailerUrl, setTrailerUrl] = useState("");
+  useEffect(() => {
+    dispatch(getUpcomingMovies());
+  }, [dispatch]);
 
-    useEffect(() => {
-        dispatch(getUpcomingMovies());
-    }, [dispatch]);
+  if (loading) {
+    return <Text>Loading...</Text>;
+  }
 
-    const sortedMovies = upcomingMovies
-    ? [...upcomingMovies].sort(
-        (a, b) =>
-          new Date(a["release-dateIS"]) - new Date(b["release-dateIS"])
-      )
-    : [];
+  if (error) {
+    return <Text>Error: {error}</Text>;
+  }
 
-    const handleWatchTrailer = (item) => {
-        const trailer =
-            item.trailers &&
-            item.trailers[0]?.results &&
-            item.trailers[0].results[0]?.url;
+  const sortedMovies = [...upcomingMovies].sort(
+    (a, b) => new Date(a["release-dateIS"]) - new Date(b["release-dateIS"])
+  );
 
-        if (trailer) {
-            setTrailerUrl(trailer);
-            setModalVisible(true);
-        } else {
-            Alert.alert("Trailer not available", "No trailer is available for this movie.");
-        }
+  const handleWatchTrailer = (item) => {
+    const trailer =
+      item.trailers &&
+      item.trailers[0]?.results &&
+      item.trailers[0].results[0]?.url;
+    if (trailer) {
+      setTrailerUrl(trailer);
+      setModalVisible(true);
+    } else {
+      Alert.alert('Trailer not available', 'No trailer is available for this movie.');
+    }
   };
-    
 
-    return (
-        <View>
-            <FlatList
-                data={sortedMovies}
-                keyExtractor={(item) => item.id.toString()}
-                renderItem={({item}) => (
-                    <TouchableOpacity style={styles.movieItem}>
-                        <Image style={styles.thumbnail} src={item.poster} />
-                        <TouchableOpacity style={styles.movieItem2}>
-                            <Text style={styles.movieName}>{item.title}</Text>
-                            <Text style={styles.movieYear}>Release Date: {item["release-dateIS"]}</Text>
-                            <Button style={styles.button} title="Watch Trailer" onPress= {() => handleWatchTrailer(item)} />
-                        </TouchableOpacity>
-                    </TouchableOpacity>
-                )}
-            />
-            <Modal
-                visible={isModalVisible}
-                transparent={true}
-                onRequestClose={() => setModalVisible(false)}
-                >
-                <View style={styles.modalContainer}>
-                <Video
-                    source={{ uri: trailerUrl }}
-                    style={styles.videoPlayer}
-                    controls
-                    resizeMode="contain"
-                />
-                <Button title="Close" onPress={() => setModalVisible(false)} />
-                </View>
-        </Modal>
+  return (
+    <View>
+      {sortedMovies.map((item) => (
+        <TouchableOpacity key={item.id} style={styles.movieItem}>
+          <Image style={styles.thumbnail} source={{ uri: item.poster }} />
+          <View style={styles.movieItem2}>
+            <Text style={styles.movieName}>{item.title}</Text>
+            <Text style={styles.movieYear}>Release Date: {item["release-dateIS"]}</Text>
+            <Button title="Watch Trailer" onPress={() => handleWatchTrailer(item)} />
+          </View>
+        </TouchableOpacity>
+      ))}
+
+      <Modal visible={isModalVisible} transparent={true} onRequestClose={() => setModalVisible(false)}>
+        <View style={styles.modalContainer}>
+          <YouTubeVideoPlayer trailerUrl={trailerUrl} />
+          <Button title="Close" onPress={() => setModalVisible(false)} />
         </View>
-    )
+      </Modal>
+    </View>
+  );
 };
-
 
 export default UpcomingMoviesScreen;
